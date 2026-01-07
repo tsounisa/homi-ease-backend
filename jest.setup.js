1766dc6d2aac242f923c1cb925866267f00df05c
tsounisa <tsounisa@ece.auth.gr>
2026-01-07 15:54:39 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,9 @@
+/* global afterAll */
+
 // This file runs after all test files have completed.
-// It is the standard fix for persistent Supertest/TCPSERVERWRAP handles 
+// It is the standard fix for persistent Supertest/TCPSERVERWRAP handles
 // that Jest fails to detect automatically in ES Module environments.
 afterAll(() => {
-    // We force a brief pause to allow all asynchronous I/O (like Supertest's closing connections) to finish.
-    return new Promise(resolve => setTimeout(resolve, 500));
+  // We force a brief pause to allow all asynchronous I/O to finish.
+  return new Promise((resolve) => setTimeout(resolve, 500));
 });
